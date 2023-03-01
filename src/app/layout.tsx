bfd7f05d1c59765a8512358ab2f7131ec0216574
engsekine <engsekine@gmail.com>
2023-03-01 20:54:38 +0900

    header、footerコンポーネント化、prettier設定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,21 @@
-import '@/styles/style.scss'
-import { Inter } from 'next/font/google'
-const inter = Inter({ subsets: ['latin'] })
+import "@/styles/style.scss"
+import { Header, Footer } from "../components/index"
+import { Inter } from "next/font/google"
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
-  title: '株式会社ゆめみフロントエンドコーディング試験',
-  description: '株式会社ゆめみフロントエンドコーディング試験です',
+  title: "株式会社ゆめみフロントエンドコーディング試験",
+  description: "株式会社ゆめみフロントエンドコーディング試験です",
 }
 
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ja" className={inter.className}>
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,25 @@
-'use client'
-import React from 'react'
-import axios from "axios";
-const API_BASEURL = "https://opendata.resas-portal.go.jp/api/v1/";
+"use client"
+import React from "react"
+import axios from "axios"
+const API_BASEURL = "https://opendata.resas-portal.go.jp/api/v1/"
 const config = {
-  headers:{
-    'X-API-KEY': process.env.NEXT_PUBLIC_RESAS_API_KEY,
-  }
-};
-
+  headers: {
+    "X-API-KEY": process.env.NEXT_PUBLIC_RESAS_API_KEY,
+  },
+}
 
 export default function Home() {
-  const [prefectures, setPostPrefectures] = React.useState(null);
+  const [prefectures, setPostPrefectures] = React.useState(null)
   React.useEffect(() => {
     axios.get(API_BASEURL + "prefectures", config).then((response) => {
-      setPostPrefectures(response.data.result);
-    });
-  }, []);
-  if (!prefectures) return null;
+      setPostPrefectures(response.data.result)
+      console.log(Object.values(response.data.result))
+    })
+  }, [])
+  if (!prefectures) return null
   return (
-    <>
-    <header className="header">
-      <h1>株式会社ゆめみフロントエンドコーディング試験</h1>
-    </header>
     <main>
       <h2>都道府県リスト</h2>
     </main>
-    <footer className="footer">
-      <small>@Daichi Sekine</small>
-    </footer>
-    </>
   )
 }
