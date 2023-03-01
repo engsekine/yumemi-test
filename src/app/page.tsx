4ccda848838293b30722efa1bc6f03f7543b9ae0
engsekine <engsekine@gmail.com>
2023-03-01 20:15:39 +0900

    都道府県取得実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,29 @@
-import React from "react";
+'use client'
+import React from 'react'
+import axios from "axios";
+const API_BASEURL = "https://opendata.resas-portal.go.jp/api/v1/";
+const config = {
+  headers:{
+    'X-API-KEY': process.env.NEXT_PUBLIC_RESAS_API_KEY,
+  }
+};
+
 
 export default function Home() {
+  const [prefectures, setPostPrefectures] = React.useState(null);
+  React.useEffect(() => {
+    axios.get(API_BASEURL + "prefectures", config).then((response) => {
+      setPostPrefectures(response.data.result);
+    });
+  }, []);
+  if (!prefectures) return null;
   return (
     <>
     <header className="header">
       <h1>株式会社ゆめみフロントエンドコーディング試験</h1>
     </header>
     <main>
-      <div className="App">
-      </div>
+      <h2>都道府県リスト</h2>
     </main>
     <footer className="footer">
       <small>@Daichi Sekine</small>
