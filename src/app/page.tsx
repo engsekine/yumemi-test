ff3093da4d2fb4ecb7d943d29fc92764060abad1
engsekine <engsekine@gmail.com>
2023-03-02 14:05:16 +0900

    人口取得完了

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import axios from "axios"
 type CheckBox = {
   prefCode: number
   prefName: string
+  check: boolean
 }
 
 const API_BASEURL = "https://opendata.resas-portal.go.jp/api/v1/"
@@ -13,23 +14,63 @@ const config = {
     "X-API-KEY": process.env.NEXT_PUBLIC_RESAS_API_KEY,
   },
 }
+type PrefPopulation = {
+  prefName: string
+  data: { year: number; value: number }
+}
 
 export default function Home() {
   const [prefectures, setPostPrefectures] = useState<CheckBox[]>([])
   useEffect(() => {
-    axios.get(API_BASEURL + "prefectures", config).then((response) => {
-      setPostPrefectures(response.data.result)
-      console.log(Object.values(response.data.result))
-    })
+    axios
+      .get(API_BASEURL + "prefectures", config)
+      .then((response) => {
+        setPostPrefectures(response.data.result)
+        console.log(Object.values(response.data.result))
+      })
+      .catch((e: unknown) => {
+        return null
+      })
   }, [])
-  if (!prefectures) return null
+
+  const [prefPopulation, setPrefPopulation] = useState<PrefPopulation[]>([])
+  function prefectureCheck(prefName: string, prefCode: number, check: boolean) {
+    let checkPrefPopulation = prefPopulation.slice()
+    if (check) {
+      axios
+        .get(API_BASEURL + "population/composition/perYear?prefCode=" + String(prefCode), config)
+        .then((response) => {
+          checkPrefPopulation.push({
+            prefName: response.data.prefName,
+            data: response.data.result.data[0].data,
+          })
+          setPrefPopulation(checkPrefPopulation)
+          console.log(checkPrefPopulation)
+        })
+        .catch((e: unknown) => {
+          return
+        })
+    } else {
+    }
+    console.log(check)
+    console.log(prefName)
+    console.log(prefCode)
+  }
+
   return (
     <main>
       <h2>都道府県リスト</h2>
       <div>
         {prefectures.map((prefecture: CheckBox) => (
           <label key={prefecture.prefName}>
-            <input type="checkbox" name="PrefectureName" />
+            <input
+              type="checkbox"
+              name="PrefectureName"
+              id={"checkbox" + prefecture.prefCode}
+              onChange={(event) =>
+                prefectureCheck(prefecture.prefName, prefecture.prefCode, event.target.checked)
+              }
+            />
             {prefecture.prefName}
           </label>
         ))}
