032e936cc8fbfb9be8a3b1e34f8a02fdfb105d62
engsekine <engsekine@gmail.com>
2023-03-02 17:13:22 +0900

    import修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client"
-import React, { useEffect, useState } from "react"
-import axios from "axios"
+import React from "react"
 import { PrefPopulation } from "@/components/index"
 
 export default function Home() {
