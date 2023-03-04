ee199b10169a7f00c54b3c38f059f0d177abb408
engsekine <engsekine@gmail.com>
2023-03-04 12:46:05 +0900

    レスポンシブデザイン完成

--- src/components/prefPopulation.tsx
+++ src/components/prefPopulation.tsx
@@ -105,9 +105,9 @@ export function PrefPopulation() {
   }
 
   return (
-    <div>
-      <h2>都道府県リスト</h2>
-      <div>
+    <div className="prefectures">
+      <h2>都道府県</h2>
+      <div className="prefectures-list">
         {prefectures.map((prefecture: CheckBox) => (
           <label key={prefecture.prefName}>
             <input
